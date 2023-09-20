27559fcd253bbee077729f80e9bde3b9dc69a02e
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2023-09-20 22:02:49 +0300

    qq

---
+++ 6kyu/break-camel-case.js
@@ -0,0 +1,12 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+// Example
+
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// My solution
+
+function solution(string) {
+    return string.replace(/([A-Z])/g, ' $1');
+}
