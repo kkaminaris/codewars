49c2b5c2e8196f336cb907521f4480372f2fa382
KaminarisKonstantinos <kaminariskonstantinos@gmail.com>
2023-11-18 12:29:40 +0200

    Create regex-password-validation.js

---
+++ 5kyu/regex-password-validation.js
@@ -0,0 +1,12 @@
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+//     At least six characters long
+//     contains a lowercase letter
+//     contains an uppercase letter
+//     contains a digit
+//     only contains alphanumeric characters (note that '_' is not alphanumeric)
+
+// My solution
+
+// assign your RegExp to REGEXP:
+const REGEXP = /(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=\D*\d)^([a-zA-Z0-9]{6,}$)/;
